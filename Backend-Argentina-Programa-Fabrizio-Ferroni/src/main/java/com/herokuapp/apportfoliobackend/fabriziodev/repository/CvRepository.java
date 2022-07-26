e3c4526408da32ff5fa8bc337ae333d2a1dbcbf2
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2022-07-26 11:30:40 -0300

    Se sube proyecto de front y back juntos

---
+++ Backend-Argentina-Programa-Fabrizio-Ferroni/src/main/java/com/herokuapp/apportfoliobackend/fabriziodev/repository/CvRepository.java
@@ -0,0 +1,25 @@
+package com.herokuapp.apportfoliobackend.fabriziodev.repository;
+
+import com.herokuapp.apportfoliobackend.fabriziodev.entity.Cv;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CvRepository extends JpaRepository<Cv, Integer> {
+    List<Cv> findAllByOrderByIdDesc();
+
+    @Query(nativeQuery = true, value = "SELECT * FROM cv ORDER BY id DESC LIMIT 1")
+    List<Cv> findLastRegister();
+
+    @Modifying
+    @Query(nativeQuery = true, value = "DELETE FROM download_cv WHERE cv_id=:id")
+    void deletedown_cv(Integer id);
+
+    @Query(nativeQuery = true, value = "SELECT COUNT(*) FROM `download_cv` WHERE cv_id=:id")
+    int exitsindown_cv(Integer id);
+
+}
